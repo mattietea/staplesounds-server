33114315be87e42819fa4916da5bd6ab26018334
mattietea <mattcthomas@me.com>
2017-02-02 09:56:34 -0500

    remote method

--- common/models/song.js
+++ common/models/song.js
@@ -1,5 +1,60 @@
 'use strict';
 
 module.exports = function(Song) {
+  Song.findByGenre = function (genres, cb) {
+    var query = {
+      where: {
+        and: []
+      },
+      order: "created DESC"
+    };
 
+    var genreQuery = {
+      pop: {'genres.pop': {exists: true}},
+      rnb: {'genres.rnb': {exists: true}},
+      hipHop: {'genres.hipHop': {exists: true}},
+
+      house: {'genres.house': {exists: true}},
+      electro: {'genres.electro': {exists: true}},
+      techno: {'genres.techno': {exists: true}},
+
+      dubstep: {'genres.dubstep': {exists: true}},
+      future: {'genres.future': {exists: true}},
+      trap: {'genres.trap': {exists: true}},
+
+      dnb: {'genres.dnb': {exists: true}},
+      beats: {'genres.beats': {exists: true}},
+      ambient: {'genres.ambient': {exists: true}},
+      bass: {'genres.bass': {exists: true}},
+
+      tropical: {'genres.tropical': {exists: true}},
+      funk: {'genres.funk': {exists: true}},
+      rock: {'genres.rock': {exists: true}},
+      indie: {'genres.indie': {exists: true}}
+    };
+
+    genres.forEach(function (genre) {
+      query.where.and.push(genreQuery[genre]);
+    });
+
+    if (genres.length == 0 || genres == 'undefined') {
+      Song.find({order: "created DESC"}, function (err, instance) {
+        cb(null, instance);
+      });
+    } else {
+      Song.find(query, function (err, instance) {
+        cb(null, instance);
+      });
+    }
+
+  };
+
+  Song.remoteMethod(
+    'findByGenre',
+    {
+      http: {path: '/findByGenre', verb: 'get'},
+      accepts: {arg: 'genres', type: 'array'},
+      returns: {type: 'array', root: true}
+    }
+  );
 };
